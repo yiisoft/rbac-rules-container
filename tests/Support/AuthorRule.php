254b23bb73d0c259af6499f8ed8ff74f96cef86c
Alexey Rogachev <arogachev90@gmail.com>
2024-03-07 15:44:37 +0500

    Update composer.json (#37)
    * Update composer.json
    
    * Actualize

--- tests/Support/AuthorRule.php
+++ tests/Support/AuthorRule.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Rbac\Rules\Container\Tests\Support;
 
 use Yiisoft\Rbac\Item;
+use Yiisoft\Rbac\RuleContext;
 use Yiisoft\Rbac\RuleInterface;
 
 /**
@@ -12,8 +13,8 @@ use Yiisoft\Rbac\RuleInterface;
  */
 final class AuthorRule implements RuleInterface
 {
-    public function execute(string $userId, Item $item, array $parameters = []): bool
+    public function execute(?string $userId, Item $item, RuleContext $context): bool
     {
-        return $parameters['authorID'] === $userId;
+        return $context->getParameterValue('authorId') === $userId;
     }
 }
